829161fe3d7792edccf9bf12c8a9ddfbd2d79f31
Daniel McCarthy <dansblackberry2@hotmail.co.uk>
2025-02-22 12:44:26 +0000

    Lecture 9 - Restoring simple terminal functionality

--- src/kernel.c
+++ src/kernel.c
@@ -1,12 +1,12 @@
 #include "kernel.h"
-// #include <stddef.h>
-// #include <stdint.h>
+#include <stddef.h>
+#include <stdint.h>
 // #include "idt/idt.h"
 // #include "memory/heap/kheap.h"
 // #include "memory/paging/paging.h"
 // #include "memory/memory.h"
 // #include "keyboard/keyboard.h"
-// #include "string/string.h"
+#include "string/string.h"
 // #include "isr80h/isr80h.h"
 // #include "task/task.h"
 // #include "task/process.h"
@@ -16,88 +16,88 @@
 // #include "disk/streamer.h"
 // #include "task/tss.h"
 // #include "gdt/gdt.h"
-// #include "config.h"
-// #include "status.h"
+#include "config.h"
+#include "status.h"
 
-// uint16_t* video_mem = 0;
-// uint16_t terminal_row = 0;
-// uint16_t terminal_col = 0;
+uint16_t* video_mem = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_col = 0;
 
-// uint16_t terminal_make_char(char c, char colour)
-// {
-//     return (colour << 8) | c;
-// }
+uint16_t terminal_make_char(char c, char colour)
+{
+    return (colour << 8) | c;
+}
 
-// void terminal_putchar(int x, int y, char c, char colour)
-// {
-//     video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, colour);
-// }
+void terminal_putchar(int x, int y, char c, char colour)
+{
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, colour);
+}
 
-// void terminal_backspace()
-// {
-//     if (terminal_row == 0 && terminal_col == 0)
-//     {
-//         return;
-//     }
+void terminal_backspace()
+{
+    if (terminal_row == 0 && terminal_col == 0)
+    {
+        return;
+    }
 
-//     if (terminal_col == 0)
-//     {
-//         terminal_row -= 1;
-//         terminal_col = VGA_WIDTH;
-//     }
+    if (terminal_col == 0)
+    {
+        terminal_row -= 1;
+        terminal_col = VGA_WIDTH;
+    }
 
-//     terminal_col -=1;
-//     terminal_writechar(' ', 15);
-//     terminal_col -=1;
-// }
+    terminal_col -=1;
+    terminal_writechar(' ', 15);
+    terminal_col -=1;
+}
 
-// void terminal_writechar(char c, char colour)
-// {
-//     if (c == '\n')
-//     {
-//         terminal_row += 1;
-//         terminal_col = 0;
-//         return;
-//     }
+void terminal_writechar(char c, char colour)
+{
+    if (c == '\n')
+    {
+        terminal_row += 1;
+        terminal_col = 0;
+        return;
+    }
 
-//     if (c == 0x08)
-//     {
-//         terminal_backspace();
-//         return;
-//     }
+    if (c == 0x08)
+    {
+        terminal_backspace();
+        return;
+    }
 
-//     terminal_putchar(terminal_col, terminal_row, c, colour);
-//     terminal_col += 1;
-//     if (terminal_col >= VGA_WIDTH)
-//     {
-//         terminal_col = 0;
-//         terminal_row += 1;
-//     }
-// }
-// void terminal_initialize()
-// {
-//     video_mem = (uint16_t*)(0xB8000);
-//     terminal_row = 0;
-//     terminal_col = 0;
-//     for (int y = 0; y < VGA_HEIGHT; y++)
-//     {
-//         for (int x = 0; x < VGA_WIDTH; x++)
-//         {
-//             terminal_putchar(x, y, ' ', 0);
-//         }
-//     }   
-// }
+    terminal_putchar(terminal_col, terminal_row, c, colour);
+    terminal_col += 1;
+    if (terminal_col >= VGA_WIDTH)
+    {
+        terminal_col = 0;
+        terminal_row += 1;
+    }
+}
+void terminal_initialize()
+{
+    video_mem = (uint16_t*)(0xB8000);
+    terminal_row = 0;
+    terminal_col = 0;
+    for (int y = 0; y < VGA_HEIGHT; y++)
+    {
+        for (int x = 0; x < VGA_WIDTH; x++)
+        {
+            terminal_putchar(x, y, ' ', 0);
+        }
+    }   
+}
 
 
 
-// void print(const char* str)
-// {
-//     size_t len = strlen(str);
-//     for (int i = 0; i < len; i++)
-//     {
-//         terminal_writechar(str[i], 15);
-//     }
-// }
+void print(const char* str)
+{
+    size_t len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        terminal_writechar(str[i], 15);
+    }
+}
 
 
 // static struct paging_4gb_chunk* kernel_chunk = 0;
@@ -127,7 +127,12 @@
 
 void kernel_main()
 {
-    // terminal_initialize();
+     terminal_initialize();
+     print("Hello 64-bit!\n");
+
+     // OLD CODE BELOW
+     // ----------------------------------
+
     // memset(gdt_real, 0x00, sizeof(gdt_real));
     // gdt_structured_to_gdt(gdt_real, gdt_structured, PEACHOS_TOTAL_GDT_SEGMENTS);
 
