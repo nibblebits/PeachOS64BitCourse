3bc381d37e1a15bac2ca85defc4c1d10e90bbbc6
Daniel McCarthy <dansblackberry2@hotmail.co.uk>
2025-04-29 15:01:33 +0100

    Lecture 37 - Remaking idt.c to work with 64-bit code

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -4,6 +4,7 @@
 #include "memory/memory.h"
 #include "task/task.h"
 #include "task/process.h"
+#include "memory/heap/kheap.h"
 #include "io/io.h"
 #include "status.h"
 struct idt_desc idt_descriptors[PEACHOS_TOTAL_INTERRUPTS];
@@ -43,14 +44,23 @@ void idt_zero()
     print("Divide by zero error\n");
 }
 
+// int 20h 
 void idt_set(int interrupt_no, void* address)
 {
-    struct idt_desc* desc = &idt_descriptors[interrupt_no];
-    desc->offset_1 = (uint32_t) address & 0x0000ffff;
-    desc->selector = KERNEL_CODE_SELECTOR;
-    desc->zero = 0x00;
-    desc->type_attr = 0xEE;
-    desc->offset_2 = (uint32_t) address >> 16;
+   struct idt_desc* desc = &idt_descriptors[interrupt_no];
+   uintptr_t _address = (uintptr_t) address;
+   desc->offset_1 = _address & 0x000000000000ffff;
+   desc->selector = KERNEL_LONG_MODE_CODE_SELECTOR;
+   desc->ist = 0;
+
+   desc->type_attr = 0xEE;
+   if (interrupt_no <= 0x31)
+   {
+      desc->type_attr = 0x8E;
+   }
+
+   desc->offset_2 = (_address >> 16) & 0x000000000000ffff;
+   desc->offset_3 = (_address >> 32) & 0x00000000ffffffff;
 }
 
 void idt_handle_exception()
@@ -71,7 +81,7 @@ void idt_init()
 {
     memset(idt_descriptors, 0, sizeof(idt_descriptors));
     idtr_descriptor.limit = sizeof(idt_descriptors) -1;
-    idtr_descriptor.base = (uint32_t) idt_descriptors;
+    idtr_descriptor.base = (uint64_t) idt_descriptors;
 
     for (int i = 0; i < PEACHOS_TOTAL_INTERRUPTS; i++)
     {
