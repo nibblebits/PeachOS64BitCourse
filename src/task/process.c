13f4ceafce711c8f7bccef44d8fef883b276ed14
Daniel McCarthy <dansblackberry2@hotmail.co.uk>
2025-05-07 14:33:51 +0100

    Lecture 42 - Rebuilding the process system to be 64-bit - extra

--- src/task/process.c
+++ src/task/process.c
@@ -457,7 +457,7 @@ int process_map_memory(struct process* process)
     }
 
     // Finally map the stack
-    paging_map_to(process->task->page_directory, (void*)PEACHOS_PROGRAM_VIRTUAL_STACK_ADDRESS_END, process->stack, paging_align_address(process->stack+PEACHOS_USER_PROGRAM_STACK_SIZE), PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL | PAGING_IS_WRITEABLE);
+    paging_map_to(process->task->paging_desc, (void*)PEACHOS_PROGRAM_VIRTUAL_STACK_ADDRESS_END, process->stack, paging_align_address(process->stack+PEACHOS_USER_PROGRAM_STACK_SIZE), PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL | PAGING_IS_WRITEABLE);
 out:
     return res;
 }
