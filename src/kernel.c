d3ba5e85463fdfbe2d26fc27994ff73ae9f54cb5
Daniel McCarthy <dansblackberry2@hotmail.co.uk>
2025-05-21 15:24:17 +0100

    Lecture 63 - Re-enabling ELF files and making the loader work on long mode

--- src/kernel.c
+++ src/kernel.c
@@ -212,7 +212,7 @@ void kernel_main()
 
     print("Loading program...\n");
     struct process* process = 0;
-    int res = process_load_switch("0:/simple.bin", &process);
+    int res = process_load_switch("0:/blank.elf", &process);
     if (res != PEACHOS_ALL_OK)
     {
         panic("Failed to load user program\n");
--- src/task/process.c
+++ src/task/process.c
@@ -375,20 +375,18 @@ out:
 
 static int process_load_elf(const char* filename, struct process* process)
 {
-    // Temporary disabled.
-    return -EINFORMAT;
-//     int res = 0;
-//     struct elf_file* elf_file = 0;
-//     res = elf_load(filename, &elf_file);
-//     if (ISERR(res))
-//     {
-//         goto out;
-//     }
+    int res = 0;
+    struct elf_file* elf_file = 0;
+    res = elf_load(filename, &elf_file);
+    if (ISERR(res))
+    {
+        goto out;
+    }
 
-//     process->filetype = PROCESS_FILETYPE_ELF;
-//     process->elf_file = elf_file;
-// out:
-//     return res;
+    process->filetype = PROCESS_FILETYPE_ELF;
+    process->elf_file = elf_file;
+out:
+    return res;
 }
 static int process_load_data(const char* filename, struct process* process)
 {
@@ -411,28 +409,27 @@ int process_map_binary(struct process* process)
 
 static int process_map_elf(struct process* process)
 {
-    //int res = 0;
-    return -EINVARG;
+    int res = 0;
 
-    // struct elf_file* elf_file = process->elf_file;
-    // struct elf_header* header = elf_header(elf_file);
-    // struct elf32_phdr* phdrs = elf_pheader(header);
-    // for (int i = 0; i < header->e_phnum; i++)
-    // {
-    //     struct elf32_phdr* phdr = &phdrs[i];
-    //     void* phdr_phys_address = elf_phdr_phys_address(elf_file, phdr);
-    //     int flags = PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL;
-    //     if (phdr->p_flags & PF_W)
-    //     {
-    //         flags |= PAGING_IS_WRITEABLE;
-    //     }
-    //     res = paging_map_to(process->task->paging_desc, paging_align_to_lower_page((void*)phdr->p_vaddr), paging_align_to_lower_page(phdr_phys_address), paging_align_address(phdr_phys_address+phdr->p_memsz), flags);
-    //     if (ISERR(res))
-    //     {
-    //         break;
-    //     }
-    // }
-    // return res;
+    struct elf_file* elf_file = process->elf_file;
+    struct elf_header* header = elf_header(elf_file);
+    struct elf32_phdr* phdrs = elf_pheader(header);
+    for (int i = 0; i < header->e_phnum; i++)
+    {
+        struct elf32_phdr* phdr = &phdrs[i];
+        void* phdr_phys_address = elf_phdr_phys_address(elf_file, phdr);
+        int flags = PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL;
+        if (phdr->p_flags & PF_W)
+        {
+            flags |= PAGING_IS_WRITEABLE;
+        }
+        res = paging_map_to(process->task->paging_desc, paging_align_to_lower_page((void*)(uintptr_t)phdr->p_vaddr), paging_align_to_lower_page(phdr_phys_address), paging_align_address(phdr_phys_address+phdr->p_memsz), flags);
+        if (ISERR(res))
+        {
+            break;
+        }
+    }
+    return res;
 }
 int process_map_memory(struct process* process)
 {
